2e8ea2f0712a6eab96f526bee92f8ba9282bf255
Allen A Le <allenanle16@gmail.com>
2018-01-24 14:13:58 -0800

    Hello World!

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App = () => (
+  <div>
+    <h1 style={{ textAlign: 'center' }}>
+      HELLO WORLD!
+    </h1>
+  </div>
+)
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const SRC_DIR = path.join(__dirname, '/src')
 const DIST_DIR = path.join(__dirname, '/dist')
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${SRC_DIR}/index.js`,
 
   output: {
     path: `${DIST_DIR}/js`,
