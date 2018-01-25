f77813bcbb6feb7fe0c790ffeb1b86c2508a3a56
Allen A Le <allenanle16@gmail.com>
2018-01-24 16:22:02 -0800

    Create AddQuestionButton component

---
+++ src/components/AddQuestionButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const AddQuestion = ({ addQuestion }) => (
+  <div>
+    <button
+      onClick={ () => addQuestion({ parentId: null }) }
+    >
+      ADD QUESTION
+    </button>
+  </div>
+)
+
+export default AddQuestion
