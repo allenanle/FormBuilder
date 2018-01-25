56761b178a7aba9135061c205c33c3bc269af62b
Allen A Le <allenanle16@gmail.com>
2018-01-24 16:37:54 -0800

    Provide 'questions' & 'addQuestion' to App's props

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { addQuestion } from '../actions/questions'
 
 
-const App = () => (
+let App = () => (
   <div>
     <h1 style={{ textAlign: 'center' }}>
       HELLO WORLD!
     </h1>
   </div>
 )
+
+const mapStateToProps = (state) => ({
+  questions: state.questions
+})
+
+const mapDispatchToProps = {
+  addQuestion
+}
+
+App = connect(mapStateToProps, mapDispatchToProps)(App)
 
 export default App
