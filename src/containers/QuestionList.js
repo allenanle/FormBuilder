601a41af425b8e92b103aa494091d48950e56ef5
Allen A Le <allenanle16@gmail.com>
2018-01-25 17:16:48 -0800

    Modify QuestionList for readability

--- src/containers/QuestionList.js
+++ src/containers/QuestionList.js
@@ -6,37 +6,34 @@ import Question from '../components/Question'
 
 
 let QuestionList = (props) => (
+  !props.isSubInputList ?
+  // QUESTION LIST
   <div>
-    {/* QUESTION LIST */}
-    { !props.isSubInputList &&
-      Object.keys(props.questions).map(id => {
-        if (props.questions[id].parentId === null) {
-          return (
-            <Question
-              key={ id }
-              questions={ props.questions }
-              addQuestion={ props.addQuestion }
-              { ...props.questions[id] }
-            />
-          )
-        }
+    { Object.keys(props.questions).map(id => {
+      if (props.questions[id].parentId === null) {
+        return (
+          <Question
+            key={ id }
+            questions={ props.questions }
+            addQuestion={ props.addQuestion }
+            { ...props.questions[id] }
+          />
+        )
+      }
     }) }
-
-    {/* SUB-INPUT LIST */}
-    { props.isSubInputList &&
-      <div className='sub-input-list'>
-        { props.subs.map(id => {
-          return (
-            <Question
-              key={ id }
-              questions={ props.questions }
-              addQuestion={ props.addQuestion }
-              { ...props.questions[id] }
-            />
-          )
-        }) }
-      </div>
-    }
+  </div> :
+  // SUB-INPUT LIST
+  <div className='sub-input-list'>
+    { props.subs.map(id => {
+      return (
+        <Question
+          key={ id }
+          questions={ props.questions }
+          addQuestion={ props.addQuestion }
+          { ...props.questions[id] }
+        />
+      )
+    }) }
   </div>
 )
 
