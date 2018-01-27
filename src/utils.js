726e071f745929064e921791886bb3c5bfa1b367
Allen A Le <allenanle16@gmail.com>
2018-01-26 20:07:38 -0800

    Modify deleteInputs util function to check if selected input is a sub

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,24 @@
 const deleteInputs = (id, state) => {
+  // delete its children
   if (state[id].subs.length) {
     state[id].subs.forEach(subId => {
       deleteInputs(subId, state)
     })
   }
 
-  const parentSubs = state[state[id].parentId].subs
-  for (let i = 0; i < parentSubs.length; i++) {
-    if (parentSubs[i] === id) {
-      parentSubs.splice(i, 1)
-      break
+  // remove its ID from its parent's subs array
+  if (state[id].parentId !== null) {
+    const parentSubs = state[state[id].parentId].subs
+    console.log(parentSubs)
+    for (let i = 0; i < parentSubs.length; i++) {
+      if (parentSubs[i] === id) {
+        parentSubs.splice(i, 1)
+        break
+      }
     }
   }
 
+  // delete itself
   delete state[id]
 }
 
