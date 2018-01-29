b200e6ad21ade19880fa2132f58a50f2eb160b1b
Allen A Le <allenanle16@gmail.com>
2018-01-28 17:26:32 -0800

    Convert InputPreview into a stateful component to handle changes of input fields

--- src/components/InputPreview.js
+++ src/components/InputPreview.js
@@ -3,36 +3,45 @@ import React from 'react'
 import InputList from './InputList'
 
 
-const InputPreview = (props) => (
-  <div>
-    <div className='input-form-preview'>
-      {/* QUESTION */}
-        <label>{ props.questionText }</label>
-        { props.questionType === 'yes/no' &&
-          <div>
-            <input type='radio' value='yes' />
-            <label>Yes</label>
-            <input type='radio' value='no' />
-            <label>No</label>
-          </div>
-        }
-        { (props.questionType === 'text' || props.questionType === 'number') &&
-          <div>
-            <input />
-          </div>
-        }
-    </div>
+class InputPreview extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selection: ''
+    }
+  }
 
-    {/* SUB-INPUT LIST */}
+  render() {
     <div>
-      { props.subs.length > 0 &&
-        <InputList
-          isSubInputList={ true }
-          { ...this.props }
-        />
-      }
+      <div className='input-form-preview'>
+        {/* QUESTION */}
+          <label>{ props.questionText }</label>
+          { props.questionType === 'yes/no' &&
+            <div>
+              <input type='radio' value='yes' />
+              <label> Yes</label>
+              <input type='radio' value='no' />
+              <label> No</label>
+            </div>
+          }
+          { (props.questionType === 'text' || props.questionType === 'number') &&
+            <div>
+              <input />
+            </div>
+          }
+      </div>
+
+      {/* SUB-INPUT LIST */}
+      <div className='sub-input-list'>
+        { props.subs.length > 0 &&
+          <InputList
+            isSubInputList={ true }
+            { ...props }
+          />
+        }
+      </div>
     </div>
-  </div>
-)
+  }
+}
 
 export default InputPreview
