eff15ac13ee4da95c9e83a3bc19f2693408f357b
Allen A Le <allenanle16@gmail.com>
2018-01-30 01:38:57 -0800

    Save to local storage as changes are made to store & also load from local storage as page refreshes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { store } from './configureStore'
 import App from './components/App'
+import rootReducer from './reducers'
+import { loadState, saveState } from './localStorage'
 
+
+const persistedState = loadState()
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  composeWithDevTools()
+)
+
+store.subscribe(() => {
+  saveState({
+    inputs: store.getState().inputs
+  })
+})
 
 ReactDOM.render(
   <Provider store={ store }>
